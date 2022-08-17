0c929925658648bfa2717322072e1645eebe0082
soponta <heber.sba@gmail.com>
2022-08-17 20:55:39 +0700

    Update and rename ads-post.js to ads-post-host+domain.js
    change link img

--- ads-post-host+domain.js
+++ ads-post-host+domain.js
@@ -4,10 +4,10 @@ var sec = now.getSeconds()
 var ad = sec % jumlah_banner;
 ad +=1;
 if (ad==1) {
-txt="Jago Powerpoint";
-url="https://account.ratakan.com/aff/go/suskses20?i=2710";
-alt="Jago Powerpoint";
-banner="https://botbisnis.com/wp-content/uploads/2020/08/Botbisnis-Jago-Powerpoint.jpg";
+txt="Hosting Termurah";
+url="https://idwebhost.com/aff/5661";
+alt="Banner Idwebhost";
+banner="https://member.idwebhost.com/assets/img/hostingtermurah/728x90.png";
 width="100%";
 height="100%";
 }
@@ -15,7 +15,7 @@ if (ad==2) {
 txt="DOMAIN MURAH BANGEET!";
 url="https://panel.niagahoster.co.id/ref/27488";
 alt="DOMAIN MURAH BANGEET!";
-banner="https://panel.niagahoster.co.id/banners/Set3-niagahoster-728x90.png";
+banner="https://niagaspace.sgp1.cdn.digitaloceanspaces.com/assets/images/affiliasi/banner/banner-banner-1660034794-728x90-2-.png";
 width="100%";
 height="100%";
 }
@@ -23,11 +23,11 @@ if (ad==3) {
 txt="HOSTING TERMURAH";
 url="https://panel.niagahoster.co.id/ref/27488";
 alt="HOSTING TERMURAH";
-banner="https://panel.niagahoster.co.id/banners/Set5-niagahoster-728x90.png";
+banner="https://niagaspace.sgp1.cdn.digitaloceanspaces.com/assets/images/affiliasi/banner/728-90-affiliate-starting-bisnis-online.png";
 width="100%";
 height="100%";
 }
-if (ad==3) {
+if (ad==4) {
 txt="Domain Murah";
 url="https://www.domainesia.com/?aff=12391";
 alt="Domain Murah";
@@ -35,11 +35,11 @@ banner="https://static.domainesia.com/assets/images/afiliasi/banner-1.png";
 width="100%";
 height="100%";
 }
-if (ad==3) {
+if (ad==5) {
 txt="Wordpress Hosting SUPER CEPAT!";
 url="https://idwebhost.com/aff/5661";
-alt="Wordpress Hosting SUPER CEPAT!";
-banner="https://member.idwebhost.com/assets/img/wordpresssupercepat/600x400.png";
+alt="Banner Idwebhost wordpress hosting";
+banner="https://member.idwebhost.com/assets/img/wordpresssupercepat/728x90.png";
 width="100%";
 height="100%";
 }
